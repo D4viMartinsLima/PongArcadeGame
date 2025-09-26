7fd453fb996e7951a5cf62e825be581275edd189
D4viMartinsLima <martinslimafkc@gmail.com>
2025-09-26 12:00:34 -0300

    Ball and collision implemented

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 
@@ -7,12 +9,14 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG GAME")
-screen.tracer(0)
+screen.tracer(0),
 
 #Criação dos paddle
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 
+#Criação da bola
+ball = Ball()
 
 
 
@@ -31,7 +35,26 @@ screen.onkey(l_paddle.go_down, "s")
 gameIsOn = True
 
 while gameIsOn:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    #Colisão com parede
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Colisão com o Paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detectar se a bola passou direto na direita
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #Detectar e bola passou direto na esquerda
+
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 
