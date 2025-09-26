6199aedcd603e45d7a947863728f270ac96ac1b9
D4viMartinsLima <martinslimafkc@gmail.com>
2025-09-26 08:58:50 -0300

    Screen implemented

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+
+screen.bgcolor("black")
+screen.setup(width=600, height=800)
+screen.title("PONG GAME")
+
+
+
+
+
+
+
+screen.exitonclick()
