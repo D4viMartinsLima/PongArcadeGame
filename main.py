da7e332bdcb36037a0fbd4a7d3704057cdbf7d2e
D4viMartinsLima <martinslimafkc@gmail.com>
2025-09-26 09:36:37 -0300

    Paddles moviment implemented

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 
+#Criação da Tela
 screen.bgcolor("black")
-screen.setup(width=600, height=800)
+screen.setup(width=800, height=600)
 screen.title("PONG GAME")
+screen.tracer(0)
+
+#Criação dos paddle
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
 
 
 
+
+
+
+
+#Event listener adicionado para movimentação de ambos Paddles
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+
+gameIsOn = True
+
+while gameIsOn:
+    screen.update()
 
 
 
